06471b5d6d89a64067c52a34925d4d9d46bdc3f2
Juliano Donini <juliano.donini@hotmail.com>
2017-10-26 22:55:48 -0200

    create config file

---
+++ carskit/run_project.sh
@@ -0,0 +1,12 @@
+java -cp . carskit.main.CARSKit -c \
+./setting_bpr_topN10.conf \
+./setting_bpr_topN5.conf \
+./setting_combinedreduction_top5.conf \
+./setting_combinedreduction_top10.conf \
+./setting_davibest_top5.conf ./setting_davibest_top10.conf \
+./setting_itemsplitting_topN5.conf \
+./setting_itemsplitting_topN10.conf \
+./setting_uisplitting_top5.conf \
+./setting_uisplitting_top10.conf \
+./setting_usersplitting_top5.conf \
+./setting_usersplitting_top10.conf
